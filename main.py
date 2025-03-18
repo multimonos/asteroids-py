fb9d95d12a998e08f488c04ca045aef11ae377d3
Craig Hopgood <searaig@gmail.com>
2025-03-18 12:10:16 -0400

    restrict to fps=60

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # initialize
     _ = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     bgcolor = (0, 0, 0)
 
     while True:
@@ -22,6 +25,7 @@ def main():
 
         # refresh
         pygame.display.flip()
+        _ = clock.tick(60)
 
 
 if __name__ == "__main__":
