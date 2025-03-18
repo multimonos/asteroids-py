6991846e7c98da608efeda260e9286c0bd61f6f7
Craig Hopgood <searaig@gmail.com>
2025-03-18 12:42:08 -0400

    bugfix delta time

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
         # refresh
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
